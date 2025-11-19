41792f23db4694e61b1f5bbf436c56811e09c12d
Knnn2 <jikx1993@gmail.com>
2025-11-19 09:05:04 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+const myImage = document.querySelector("img");
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/1.png") {
+    myImage.setAttribute("src", "images/2.png");
+  } else {
+    myImage.setAttribute("src", "images/1.png");
+  }
+};
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName() {
+  const myName = prompt("Please entry your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool,${myName}`;
+  }
+}
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mozilla is coll,${storedName}`;
+}
+myButton.onclick = function () {
+  setUserName();
+};
